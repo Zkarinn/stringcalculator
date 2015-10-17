23102ea7b797534627fdadf3c89c786da4273d55
zkarinn <zkarinn@gmail.com>
2015-10-17 19:59:15 +0000

    testAddDifferentDelimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -26,7 +26,16 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-		return numbers.split("[,\n]");
+		String delimiter = ",|\n";
+		if(numbers.startsWith("//")){
+			int delimiterIndex = numbers.indexOf("//") +2;
+				delimiter = delimiter + "|" + numbers.substring(delimiterIndex, delimiterIndex + 1);
+				numbers = numbers.substring(delimiterIndex + 2);
+				return numbers.split(delimiter);
+		}
+		else {
+				return numbers.split("[,\n]");
+			 }
 	}
 
 }
