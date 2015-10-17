94b0ddae20e12e029c62f92c23838a9dca8cad50
zkarinn <zkarinn@gmail.com>
2015-10-17 19:09:55 +0000

    testMultipleNumbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,8 +7,7 @@ public class Calculator {
     		return 0;
     	}
     	else if(text.contains(",")){
-		    String[] numbers = text.split(",");
-		    return toInt(numbers[0]) + toInt(numbers[1]);
+		    return sum(splitNumbers(text));
 		}
     	else
     		return 1;
@@ -16,6 +15,18 @@ public class Calculator {
 
 	private static int toInt(String number){
     		return Integer.parseInt(number);
+	}
+
+	private static int sum(String[] numbers){
+		int sum = 0;
+		for(String number : numbers){
+			sum += toInt(number);
+		}
+		return sum;
+	}
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
 	}
 
 }
